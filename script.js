90ec4b63f08b61803bdedb56a01331857af37fbb
Sanket Athawale <athawalesanket0@gmail.com>
2024-12-26 19:29:19 +0530

    Resolved toggle menu

--- script.js
+++ script.js
@@ -11,4 +11,15 @@ document.addEventListener("DOMContentLoaded", function () {
         mouseDrag: true,
         loop: true,
     });
+
+    const navLinks = document.querySelectorAll('.nav-link');
+    const navbarCollapse = document.querySelector('.navbar-collapse');
+
+    navLinks.forEach(function (link) {
+        link.addEventListener('click', function () {
+            if (navbarCollapse.classList.contains('show')) {
+                new bootstrap.Collapse(navbarCollapse).hide();
+            }
+        });
+    });
 });
