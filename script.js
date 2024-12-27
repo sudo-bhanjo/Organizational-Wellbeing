de5b2eb29222eb6f615c06a3f38e3b1aeeb1b851
Sanket Athawale <athawalesanket0@gmail.com>
2024-12-27 21:54:02 +0530

    Updated page

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
+    const navLinks = document.querySelectorAll('.nav-link');
+    const navbarCollapse = document.querySelector('.navbar-collapse');
+
+    navLinks.forEach(function (link) {
+        link.addEventListener('click', function () {
+            if (navbarCollapse.classList.contains('show')) {
+                new bootstrap.Collapse(navbarCollapse).hide();
+            }
+        });
+    });
+    
     const slider = tns({
         container: "#hero-slider",
         items: 1,
@@ -10,16 +21,5 @@ document.addEventListener("DOMContentLoaded", function () {
         autoplayButtonOutput: false,
         mouseDrag: true,
         loop: true,
-    });
-
-    const navLinks = document.querySelectorAll('.nav-link');
-    const navbarCollapse = document.querySelector('.navbar-collapse');
-
-    navLinks.forEach(function (link) {
-        link.addEventListener('click', function () {
-            if (navbarCollapse.classList.contains('show')) {
-                new bootstrap.Collapse(navbarCollapse).hide();
-            }
-        });
     });
 });
